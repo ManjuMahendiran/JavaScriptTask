f6444a8a457f8a0ceb55c02cf096741dbee2cc20
ManjuMahendiran <manju@zaigoinfotech.com>
2023-02-09 10:58:41 +0530

    Task2

--- Test1.js
+++ Test1.js
@@ -2,14 +2,13 @@ var nameArray = ['red', 'pink', 'orange', 'white']; //store the nameArray
 //check if 'orange' name available in array
 var isNameAvailable = nameArray.includes('orange');
 
-if(isNameAvailable) {
-  console.log('orange name is available in given array');
-  
+if (isNameAvailable) {
+	console.log('orange name is available in given array');
+
 }
-else{
-  console.log('orange name is not available in given array');
+else {
+	console.log('orange name is not available in given array');
 }
 
 
-                   
- 
+
---
+++ Test2.js
@@ -0,0 +1,14 @@
+var name = 'manju'; //store the namw in one variable
+
+var arrayOfName = name.split(''); // Convert string to array
+
+arrayOfName.forEach(function(value, index) {
+
+	if (index % 2 === 0) {// find only alternative values
+		console.log(value);
+
+	}
+
+
+});
+
