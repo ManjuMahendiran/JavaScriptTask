c136e1fa5dd8b2cdfeb91a4dd2a2d293eaff4d81
ManjuMahendiran <manju@zaigoinfotech.com>
2023-02-09 10:51:35 +0530

    Task1

---
+++ Test1.js
@@ -0,0 +1,15 @@
+var nameArray = ['red', 'pink', 'orange', 'white']; //store the nameArray
+//check if 'orange' name available in array
+var isNameAvailable = nameArray.includes('orange');
+
+if(isNameAvailable) {
+  console.log('orange name is available in given array');
+  
+}
+else{
+  console.log('orange name is not available in given array');
+}
+
+
+                   
+ 
